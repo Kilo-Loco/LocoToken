00273d946310d74656d069eb5b94249356fb64d7
Kilo Loco <kyle@kiloloco.com>
2024-04-01 23:00:40 -0700

    Finish basics of the token

--- src/LocoToken.sol
+++ src/LocoToken.sol
@@ -1,8 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LocoToken is ERC20 {
-    constructor(uint initialSupply) ERC20("Loco Token", "LOCO") {}
+    error NotOwnerError();
+
+    address private immutable i_owner;
+
+    constructor(uint initialSupply) ERC20("Loco Token", "LOCO") {
+        uint ownersShare = initialSupply / 2;
+        uint remainingShare = initialSupply / 2;
+        i_owner = msg.sender;
+        _mint(i_owner, ownersShare);
+        _mint(address(this), remainingShare);
+    }
+
+    function transferToFaucet(address faucetAddress, uint amount) external {
+        if (msg.sender != i_owner) {
+            revert NotOwnerError();
+        }
+        _transfer(msg.sender, faucetAddress, amount);
+    }
+
+    function getOwner() external view returns (address) {
+        return i_owner;
+    }
 }
